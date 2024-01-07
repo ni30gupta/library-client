6098bf653e4160b55394f4de9bce4adec03bac1f
ni30gupta <47933402+ni30gupta@users.noreply.github.com>
2024-01-07 22:51:23 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const base_url = 'http://127.0.1.1:3030/api'
+export const base_url = 'http://backend-lbr-env.eba-wdndh33j.ap-south-1.elasticbeanstalk.com/api'
  // 'https://node-server-env.eba-6gdj9hyn.ap-south-1.elasticbeanstalk.com/api' 
 
